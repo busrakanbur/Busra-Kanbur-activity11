9627887eefe2c7456d5e81a2bb14f035bcc13ee4
busrakanbur <nolensvolens99@gmail.com>
2023-06-05 19:32:42 +0300

    done

--- Kanbur-Activity11.js
+++ Kanbur-Activity11.js
@@ -1,29 +1,25 @@
 $(document).ready(function() {
-	// Click event handler for speaker links in the sidebar
-	$("#nav_list a").click(function(event) {
-		event.preventDefault(); // Prevent default link behavior
-
-		// Get the data-json attribute of the clicked link
-		var jsonFileName = "json_files/" + $(this).data("json") + ".json";
-
-		// Clear the main element
-		$("main").empty();
-
-		// Fetch JSON data for the selected speaker
-		$.getJSON(jsonFileName, function(data) {
-			var speaker = data.speakers[0];
-
-			// Create and append the speaker's information to the main element
-			var speakerTitle = $("<h1>").text(speaker.title);
-			var speakerImage = $("<img>").attr("src", "json_files/" + speaker.image);
-			var speakerMonth = $("<h2>").text(speaker.month);
-			var speakerText = $("<p>").text(speaker.text);
-
-			$("main")
-				.append(speakerTitle)
-				.append(speakerImage)
-				.append(speakerMonth)
-				.append(speakerText);
-		});
+    
+	$("#nav_list a").click(function(e) {
+		e.preventDefault();
+		var fileName = $(this).attr("title") + ".json";
+		loadSpeakerData(fileName);
 	});
+
+	function loadSpeakerData(fileName) {
+
+		$("#speakerData").empty();
+
+		$.getJSON("json_files/" + fileName, function(data) {
+
+			var speakerTitle = $("<h1>").text(data.speakers[0].title);
+			var speakerImage = $("<img>").attr("src", data.speakers[0].image);
+			var speakerMonth = $("<h2>").text(data.speakers[0].month);
+			var speakerText = $("<p>").text(data.speakers[0].text);
+
+			$("#speakerData").append(speakerTitle, speakerImage, speakerMonth, speakerText);
+           
+            
+		});
+	}
 });
